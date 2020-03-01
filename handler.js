c47351ebc81c00a42cf61df58887c3d9bea30c99
AlexMargon <alex.canton92@gmail.com>
2020-03-01 23:37:32 +0100

    Changed serverless.yml to handle the chrome and puppeteer stuff Changed handler.js handle the stuff to get the actual value from the webpage Add page-handler.js to remove ads in the webpage and the cookie consent banner

--- handler.js
+++ handler.js
@@ -1,18 +1,60 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
+const getChromeInstance = require("./getChromeInstance");
+const puppeteer = require("puppeteer-core");
+const canvas = require('canvas');
+const Clipper = require('image-clipper');
+
+module.exports.info = async event => {
+
+    const championName = event;
+
+    const chrome = await getChromeInstance();
+
+    const browser = await puppeteer.connect({
+        browserWSEndpoint: chrome.endpoint
+    });
+
+    const page = await browser.newPage();
+
+    await page.setViewport({
+        width: 1150,
+        height: 1700,
+        deviceScaleFactor: 1
+    });
+
+    await page.goto(`https://u.gg/lol/champions/${championName}/build`, {
+        waitUntil: ["domcontentloaded", "networkidle2"]
+    });
+
+    await page.addScriptTag({path: 'page-handler.js'});
+
+    const header = await page.$('.champion-profile-page');
+    const rect = await header.boundingBox();
+
+    await page.screenshot({path: 'out.png'});
+
+    Clipper.configure('canvas', canvas);
+
+    Clipper('out.png', function() {
+        this.crop(rect.x, rect.y, rect.width, rect.height)
+            .quality(80)
+            .toFile(`${championName}.jpg`, function() {
+                console.log('saved!');
+            });
+    });
+
+    await browser.close();
+
+    return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        input: championName,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
---
+++ page-handler.js
@@ -0,0 +1,5 @@
+window.__cmpui("setAndSaveAllConsent",!0);
+
+document.getElementsByClassName("ad-box-wrapper")[0].remove();
+document.getElementsByClassName("_grid-0")[0].remove();
+
